1e81bc10a35a6bad92276dd8ee61f4e0017bbff1
Elaine Liong <elaine96liong@gmail.com>
2020-02-11 20:17:16 +0800

    change textfield

--- Purchasing System/Quotation/views.py
+++ Purchasing System/Quotation/views.py
@@ -42,19 +42,7 @@ def fillingquotation(request):
     quo_id = random.randint(1000000,9999999)
     user_id  = request.user.id
     staff = Person.objects.get(user_id = user_id)
-    
-    if Quotation.objects.get(request_for_quotation_id = re_of_quo_id) is not None:
-        context = { 'error': 'Quotation for this request already exists !',
-                    'title': 'Quotation Form'
-            }
-        return render(request,'Quotation/quotationform.html',context) 
-
-    if Quotation.objects.get(quo_id = quo_id) is not None:
-        context = { 'error': 'Quotation for this request already exists !',
-                    'title': 'Quotation Form'
-            }
-        return render(request,'Quotation/quotationform.html',context) 
-
+   
     try: 
         request_for_quotations = RequestForQuotation.objects.get(request_for_quotation_id = re_of_quo_id)
         item_list = RequestForQuotationItem.objects.filter(request_for_quotation_id = re_of_quo_id)
@@ -66,6 +54,7 @@ def fillingquotation(request):
                 'vendor_id': request_for_quotations.vendor_id.vendor_id,
                 'rows':item_list
             }
+      
         return render(request,'Quotation/quotationform.html',context)
 
     except RequestForQuotation.DoesNotExist:
@@ -75,12 +64,14 @@ def fillingquotation(request):
             }
         return render(request,'Quotation/quotationform.html',context)
 
-  #  except Quotation.DoesNotExist:
-  #      context = {'error': 'The quotation id is invalid !',
-  #                 'title': 'Quotation Form'
-  #          }
-  #      return render(request,'Quotation/quotationform.html',context)
-    
+
+#    if Quotation.objects.get(request_for_quotation_id = re_of_quo_id) is not None:
+#        context = { 'error': 'Quotation for this request already exists !',
+#                   'title': 'Quotation Form'
+#           }
+
+#        return render(request,'Quotation/quotationform.html',context) 
+
 
 def quotationconfirmation(request):
 
