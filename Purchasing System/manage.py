2e78a8b28896923d8ee6c64d9be87b1749de0183
faridyusoff97 <60952608+faridyusoff97@users.noreply.github.com>
2020-02-13 20:46:00 +0800

    Update manage.py

--- Purchasing System/manage.py
+++ Purchasing System/manage.py
@@ -11,6 +11,10 @@ if __name__ == "__main__":
         "DJANGO_SETTINGS_MODULE",
         "Purchasing_System.settings"
     )
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587        
 
     from django.core.management import execute_from_command_line
 
